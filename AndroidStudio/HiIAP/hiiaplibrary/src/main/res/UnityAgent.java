9cfef045c62159c5719b3a95de99a057cb2eee25
hiram <hiramtan@qq.com>
2015-07-02 15:21:18 +0800

    no message

--- AndroidStudio/HiIAP/hiiaplibrary/src/main/res/UnityAgent.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Hiram on 2015-07-02.
- */
-public class UnityAgent {
-
-
-
-}
---
+++ AndroidStudio/MyProject/hiiap/src/main/java/hiram/hiiap/UnityAgent.java
@@ -0,0 +1,45 @@
+package hiram.hiiap;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class UnityAgent extends UnityPlayerActivity {
+
+
+
+
+
+
+    /*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unity_agent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_unity_agent, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
+}
